ca8c399d7af6384ea7be069d2be44c8711d1b58e
José Baptista <baprivas@gmail.com>
2018-11-09 21:36:32 -0400

    no viable

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -8,7 +8,27 @@ import Vue from 'vue';
        
     },
     methods: {
-        
+        nuevaEntrada (){
+            
+            var url = 'entradas'
+            console.log(this.entrada)
+            Axios.post( url, {
+                placa: this.placa,
+                marca: this.marca,
+                estado: this.estado,
+                puesto: this.puesto
+                
+            }).then(response => {
+                //getEntradas para actualizar el panel
+                this.formulario = {}
+                this.entrada = {}
+                this.errors = []
+                toastr.success('Entrada Exitosa')
+            }).catch(error => {
+                this.errors = 'Corrija para poder crear con éxito'
+            });
+            
+        }
     },
 
 });
